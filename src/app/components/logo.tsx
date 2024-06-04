706295eb3569af4190817678f901782c50bca63d
jame4005 <jame4005@gmail.com>
2024-06-04 14:20:21 +0700

    Loop jsx

---
+++ src/app/components/logo.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <div>
+        <h2>Hello World 1234</h2>
+    </div>
+  )
+}
+
+export default Logo
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Logo from "./components/logo";
 
 export default function Home() {
   return (
     <div>
-      <h2>Hello World</h2>
+      <Logo />
+      <Logo />
+      <Logo />
+      <Logo />
     </div>
   );
 }
