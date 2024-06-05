9fc4419e2af03893a01085e750c3bbca3e55d9a8
jame4005 <jame4005@gmail.com>
2024-06-05 14:06:27 +0700

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    // basePath: "/myproject" // https://nurse.cmu.ac.th/myproject
+    // poweredByHeader: false,
+    // compress: true,
+    images: {
+        minimumCacheTTL : 10,
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'www.nurse.cmu.ac.th',
+          },
+          {
+            protocol: 'https',
+            hostname: '**.cmu.ac.th',
+          },
+        ],
+      },
 };
 
 export default nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
-import Hero from "./components/hero";
-import Logo from "./components/logo";
+
+import Logo from "@/app/components/logo";  // @ คือ Folder src alias ตอนสร้าง Project ใหม่
 import styles from "./page.module.css"
 
+import Link from "next/link";
+import Image from 'next/image'
+import profilePic from '@/assets/images/me.jpg'
+
 export default function Home() {
+  const appPassword = process.env.APP_PASSWORD
   return (
     <main>
-      <h3 className="title">Hello Next.js</h3>
+      <ul style={{display:'flex', listStyle:'none'}}>
+        <li style={{padding: '14px 20px',}}><Link href="/" replace>Home</Link></li>
+        <li style={{padding: '14px 20px',}}><Link href="/about">About</Link></li>
+        <li style={{padding: '14px 20px',}}><Link href="/contact">Contact</Link></li>
+      </ul>
+
+      <Image src={profilePic} alt="me" priority></Image>
+      <hr/>
+      <h3 className="title">
+        {process.env.APP_NAME}
+      </h3>
+      {
+        appPassword == "123456" ? <p>OK ! {process.env.NEXT_PUBLIC_APP_ANALYTIC}</p> : <p>Failed !</p>
+      }
       <p className={styles.title}>Hello World</p>
       <Logo title="jame"/>
     </main>
